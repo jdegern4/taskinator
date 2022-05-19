819110408cf5c4394a5ac136914c5a500bfb06ac
Jordan D <degco44@gmail.com>
2022-05-18 22:09:07 -0500

    added form reset feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,39 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var taskFormHandler = function(event) {
-event.preventDefault ();
-var taskNameInput = document.querySelector("input[name='task-name']").value;
-var taskTypeInput = document.querySelector("select[name='task-type']").value;
+var taskFormHandler = function (event) {
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-// PACKAGE UP DATA AS AN OBJECT
-var taskDataObj = {
-    name: taskNameInput,
-    type: taskTypeInput
+    // CHECK IF INPUT VALUES ARE EMPTY STRINGS
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+    formEl.reset();
+
+    // PACKAGE UP DATA AS AN OBJECT
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    // SEND IT AS AN ARGUMENT TO createTaskEl
+    createTaskEl(taskDataObj);
 };
 
-// SEND IT AS AN ARGUMENT TO createTaskEl
-createTaskEl(taskDataObj);
-};
-
-var createTaskEl = function(taskDataObj) {
+var createTaskEl = function (taskDataObj) {
 
     // CREATE LIST ITEM
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    
+
     // CREATE DIV TO HOLD TASK INFO AND ADD TO LIST ITEM
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
-    
+
     // ADD HTML CONTENT TO DIV
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
