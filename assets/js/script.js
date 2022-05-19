32d6f6d63ce27edb548b479142461c8c0ba32d54
Jordan D <degco44@gmail.com>
2022-05-18 21:45:43 -0500

    flesh out add task function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,25 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+event.preventDefault ();
+var taskNameInput = document.querySelector("input[name='task-name']").value;
+var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    // CREATE LIST ITEM
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-}
+    
+    // CREATE DIV TO HOLD TASK INFO AND ADD TO LIST ITEM
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    
+    // ADD HTML CONTENT TO DIV
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
 
-buttonEl.addEventListener("click", createTaskHandler);
+    // ADD ENTIRE LIST ITEM TO TASK LIST
+    tasksToDoEl.appendChild(listItemEl);
+};
+
+formEl.addEventListener("submit", createTaskHandler);
