f29e201ca2a58004eff47e5444c253b067db1df2
Jordan D <degco44@gmail.com>
2022-05-23 21:33:54 -0500

    task delete and edit functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var pageContentEl = document.querySelector("#page-content");
 
 var taskFormHandler = function (event) {
     event.preventDefault();
@@ -96,3 +97,46 @@ var createTaskActions = function(taskId) {
 };
 
 formEl.addEventListener("submit", taskFormHandler);
+
+var taskButtonHandler = function(event) {
+    // GET TARGET ELEMENT FROM EVENT
+    var targetEl = event.target;
+
+    // EDIT BUTTON WAS CLICKED
+    if (targetEl.matches(".edit-btn")) {
+        var taskId = targetEl.getAttribute("data-task-id");
+        editTask(taskId);
+    }
+    // DELETE BUTTON WAS CLICKED
+    else if (targetEl.matches(".delete-btn")) {
+       var taskId = targetEl.getAttribute("data-task-id");
+       deleteTask(taskId);
+    }
+};
+
+var editTask = function(taskId) {
+    console.log("editing task #" + taskId);
+
+    // GET TASK LIST ITEM ELEMENT
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    // GET CONTENT FROM TASK NAME AND TYPE
+    var taskName = taskSelected.querySelector("h3.task-name").textContent;
+    var taskType = taskSelected.querySelector("span.task-type").textContent;
+    document.querySelector("input[name='task-name']").value = taskName;
+    document.querySelector("select[name='task-type']").value = taskType;
+    document.querySelector("#save-task").textContent = "Save Task";
+    formEl.setAttribute("data-task-id", taskId);
+};
+
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "'");
+    taskSelected.remove();
+    console.log(taskId);
+    if (event.target.matches(".delete-btn")) {
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+
+pageContentEl.addEventListener("click", taskButtonHandler);
